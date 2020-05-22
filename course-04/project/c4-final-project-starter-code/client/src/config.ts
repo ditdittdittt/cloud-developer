fa9b9a49fc6c5a557dccdb39e2c22d0ede31474b
ditdittdittt <ditdittdittt@gmail.com>
2020-05-22 12:24:34 +0700

    [yudit] finish project 4

--- course-04/project/c4-final-project-starter-code/client/src/config.ts
+++ course-04/project/c4-final-project-starter-code/client/src/config.ts
@@ -1,10 +1,10 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '...'
-export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+const apiId = 'br479mof14'
+export const apiEndpoint = `https://${apiId}.execute-api.ap-southeast-1.amazonaws.com/dev`
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'dev--iv2gmvh.auth0.com',            // Auth0 domain
+  clientId: 'YZTnrVLPwAIm6evX7c0XNi43WkFFVi1O',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
